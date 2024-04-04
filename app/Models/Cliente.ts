52c9ac78776d01e621cfa92dee619292f609ccee
Jonas <ijonasdev@gmail.com>
2024-04-04 00:00:55 -0300

    feat: novos rotinas

---
+++ app/Models/Cliente.ts
@@ -0,0 +1,23 @@
+import { BaseModel, column, hasMany, HasMany } from "@ioc:Adonis/Lucid/Orm";
+import Endereco from "App/Models/Endereco";
+import { DateTime } from "luxon";
+
+export default class Cliente extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number;
+  @column()
+  public userId: number;
+  @column()
+  public nome: string;
+  @column()
+  public telefone: string;
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime;
+
+  @hasMany(() => Endereco, {
+    localKey: "id",
+    foreignKey: "clienteId",
+  })
+  public enderecos: HasMany<typeof Endereco>;
+}
--- start/routes.ts
+++ start/routes.ts
@@ -1,10 +1,36 @@
-import Route from "@ioc:Adonis/Core/Route";
-import User from "App/Models/User";
+import Route from "@ioc:Adonis/Core/Route"; 
+
+
+// Login para os 3 tippos de user
+Route.post("/login", "AuthController.login");
+Route.post("/logout", "AuthController.logout");
+
+
+Route.post("/cliente/cadastro", "ClientesController.store");
+
+Route.get("/cidades", "CidadesController.index");
+Route.get(
+  "/cidades/:id/estabelecimentos",
+  "CidadesController.Estabelecimentos"
+);
+
+Route.group(() => {
+  Route.get("auth/me", "AuthController.me");
+
+  Route.resource("/enderecos", "EnderecosController").only([ 
+    "store",
+    "index",
+    "update",
+    "destroy",
+  ]);
+
+  Route.put("/cliente", "ClientesController.update");
+}).middleware("auth");
 
 Route.get("/", async () => {
-  User.create({
-    email: "admin@admin.com",
-    password: "123456",
-    tipo: "admin",
-  });
+  return {
+    hortifrute: "Hello World!"
+  }
 });
+
+
